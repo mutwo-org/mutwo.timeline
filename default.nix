2a730ed379edc2f2a493ca9332e86f35eab14e0c
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-11-07 19:22:43 +0100

    fixup! nix:Use derivation from mutwo-nix but \w local src

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
-  mutwo-timeline = import (sourcesTarball + "/mutwo.core/default.nix") {};
+  mutwo-timeline = import (sourcesTarball + "/mutwo.timeline/default.nix") {};
   mutwo-timeline-local = mutwo-timeline.overrideAttrs (
     finalAttrs: previousAttrs: {
        src = ./.;
